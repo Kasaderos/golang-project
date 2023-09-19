25f88d563de00939bb4d1243bf09c2efe56c2505
lcongra <kberda99@gmail.com>
2023-09-19 23:54:25 +0600

    [GOHW-1]: refactor loms usecase ifaces

--- loms/internal/usecase/interfaces.go
+++ loms/internal/usecase/interfaces.go
@@ -5,29 +5,47 @@ import (
 	"route256/loms/internal/models"
 )
 
-type WarehouseManagementSystem interface {
-	GetStockInfo(
-		ctx context.Context,
-		SKU models.SKU,
-	) (count uint64, err error)
-}
+type (
+	WarehouseManagementSystem interface {
+		GetStockInfo(
+			ctx context.Context,
+			SKU models.SKU,
+		) (count uint64, err error)
+	}
 
-type OrderManagementSystem interface {
-	CreateOrder(
-		ctx context.Context,
-		userID models.UserID,
-		info CreateOrderInfo,
-	) (models.OrderID, error)
-	GetOrderInfo(
-		ctx context.Context,
-		orderID models.OrderID,
-	) (models.Order, error)
-	MarkOrderAsPaid(
-		ctx context.Context,
-		orderID models.OrderID,
-	) error
-	CancelOrder(
-		ctx context.Context,
-		orderID models.OrderID,
-	) error
-}
+	OrderManagementSystem interface {
+		OrderCreator
+		OrderProvider
+		OrderAsPaidMarker
+		OrderCanceller
+	}
+
+	OrderCreator interface {
+		CreateOrder(
+			ctx context.Context,
+			userID models.UserID,
+			info CreateOrderInfo,
+		) (models.OrderID, error)
+	}
+
+	OrderProvider interface {
+		GetOrderInfo(
+			ctx context.Context,
+			orderID models.OrderID,
+		) (models.Order, error)
+	}
+
+	OrderAsPaidMarker interface {
+		MarkOrderAsPaid(
+			ctx context.Context,
+			orderID models.OrderID,
+		) error
+	}
+
+	OrderCanceller interface {
+		CancelOrder(
+			ctx context.Context,
+			orderID models.OrderID,
+		) error
+	}
+)
