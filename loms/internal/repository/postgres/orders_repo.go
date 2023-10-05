15d8ed12b43f631294f48e6eeed2a04f8f87bc08
Berdibek <berdibek.kaldarov@mechta.kz>
2023-10-05 18:03:43 +0600

    [GOHW-2]: add postgres folder

---
+++ loms/internal/repository/postgres/orders_repo.go
@@ -0,0 +1,49 @@
+package postgres
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"route256/loms/internal/models"
+)
+
+const OrderExpiration = time.Minute * 10
+
+type OrdersRepository struct {
+}
+
+func NewOrdersRepostiory() *OrdersRepository {
+	return &OrdersRepository{}
+}
+
+func (r *OrdersRepository) CreateOrder(
+	ctx context.Context,
+	order models.Order,
+) (models.OrderID, error) {
+	return models.OrderID(rand.Int() % 1000), nil
+}
+
+func (r *OrdersRepository) GetOrderByID(
+	ctx context.Context,
+	orderID models.OrderID,
+) (*models.Order, error) {
+	return &models.Order{
+		ID: orderID,
+	}, nil
+}
+
+func (r *OrdersRepository) SetStatus(
+	ctx context.Context,
+	orderID models.OrderID,
+	status models.Status,
+) error {
+	return nil
+}
+
+func (r *OrdersRepository) ListExpiredOrders(
+	ctx context.Context,
+	limit uint32,
+) ([]models.OrderID, error) {
+	return []models.OrderID{}, nil
+}
--- loms/migrations/20231005101700_create_order_table.sql
+++ loms/migrations/20231005101700_create_order_table.sql
@@ -2,8 +2,12 @@
 CREATE TABLE "order"
 (
   id text default gen_random_uuid() not null,
+  user_id text,
+  status text,
   primary key(id)
 );
+
+CREATE TABLE "cart"
 
 -- +goose Down
 DROP TABLE "order";
