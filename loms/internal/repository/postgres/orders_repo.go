53f56c96aa18c1c1e9b682caa335b3635ee35f14
Berdibek <kb.kaldarov@yandex.ru>
2023-10-07 16:26:17 +0600

    [GOHW-3]: add repository/postgres for loms

--- loms/internal/repository/postgres/orders_repo.go
+++ loms/internal/repository/postgres/orders_repo.go
@@ -2,34 +2,94 @@ package postgres
 
 import (
 	"context"
-	"math/rand"
-	"time"
+	"fmt"
+	"log"
 
 	"route256/loms/internal/models"
+
+	sqlc "route256/loms/internal/repository/postgres/order"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const OrderExpiration = time.Minute * 10
-
 type OrdersRepository struct {
+	dbpool *pgxpool.Pool
 }
 
-func NewOrdersRepostiory() *OrdersRepository {
-	return &OrdersRepository{}
+func NewOrdersRepostiory(dbpool *pgxpool.Pool) *OrdersRepository {
+	return &OrdersRepository{
+		dbpool: dbpool,
+	}
 }
 
 func (r *OrdersRepository) CreateOrder(
 	ctx context.Context,
 	order models.Order,
 ) (models.OrderID, error) {
-	return models.OrderID(rand.Int() % 1000), nil
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return models.OrderID(0), err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+
+	id, err := q.CreateOrder(ctx, sqlc.CreateOrderParams{
+		UserID: pgtype.Int8{
+			Int64: int64(order.UserID),
+			Valid: true,
+		},
+		Status: pgtype.Text{
+			String: order.Status.String(),
+			Valid:  true,
+		},
+	})
+	if err != nil {
+		return models.OrderID(0), fmt.Errorf("create order: %w", err)
+	}
+
+	for _, item := range order.Items {
+		if err := q.AddOrderItem(ctx, sqlc.AddOrderItemParams{
+			OrderID: pgtype.Int8{
+				Int64: id,
+				Valid: true,
+			},
+			Sku: pgtype.Int8{
+				Int64: int64(item.SKU),
+				Valid: true,
+			},
+			Count: pgtype.Int4{
+				Int32: int32(item.Count),
+				Valid: true,
+			},
+		}); err != nil {
+			return models.OrderID(0), fmt.Errorf("add order item: %w", err)
+		}
+	}
+
+	return models.OrderID(id), tx.Commit(ctx)
 }
 
 func (r *OrdersRepository) GetOrderByID(
 	ctx context.Context,
 	orderID models.OrderID,
 ) (*models.Order, error) {
+
+	q := sqlc.New(r.dbpool)
+	order, err := q.GetOrderByID(ctx, int64(orderID))
+	if err != nil {
+		return nil, err
+	}
 	return &models.Order{
-		ID: orderID,
+		ID:     orderID,
+		UserID: models.UserID(orderID),
+		Status: models.GetStatus(order.Status.String),
 	}, nil
 }
 
@@ -38,12 +98,27 @@ func (r *OrdersRepository) SetStatus(
 	orderID models.OrderID,
 	status models.Status,
 ) error {
-	return nil
-}
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
 
-func (r *OrdersRepository) ListExpiredOrders(
-	ctx context.Context,
-	limit uint32,
-) ([]models.OrderID, error) {
-	return []models.OrderID{}, nil
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+	if err := q.SetStatus(ctx, sqlc.SetStatusParams{
+		ID: int64(orderID),
+		Status: pgtype.Text{
+			String: status.String(),
+			Valid:  true,
+		},
+	}); err != nil {
+		return err
+	}
+
+	return tx.Commit(ctx)
 }
---
+++ loms/internal/repository/postgres/stocks_repo.go
@@ -0,0 +1,169 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"route256/loms/internal/models"
+
+	sqlc "route256/loms/internal/repository/postgres/stock"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type StocksRepository struct {
+	dbpool *pgxpool.Pool
+}
+
+func NewStocksRepostiory(dbpool *pgxpool.Pool) *StocksRepository {
+	return &StocksRepository{
+		dbpool: dbpool,
+	}
+}
+
+func (r *StocksRepository) ReserveStocks(
+	ctx context.Context,
+	userID models.UserID,
+	items []models.ItemOrderInfo,
+) error {
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+
+	for _, item := range items {
+		if err := q.ReserveStock(ctx, sqlc.ReserveStockParams{
+			UserID: pgtype.Int8{
+				Int64: int64(userID),
+				Valid: true,
+			},
+			Sku: pgtype.Int8{
+				Int64: int64(item.SKU),
+				Valid: true,
+			},
+			Count: pgtype.Int8{
+				Int64: int64(item.SKU),
+				Valid: true,
+			},
+		}); err != nil {
+			return fmt.Errorf("reserver stock: %w", err)
+		}
+	}
+
+	return tx.Commit(ctx)
+}
+
+func (r *StocksRepository) ReserveRemove(
+	ctx context.Context,
+	userID models.UserID,
+) error {
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+
+	stocks, err := q.GetReservedStockByUsedID(ctx, pgtype.Int8{
+		Int64: int64(userID),
+		Valid: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, stock := range stocks {
+		if err := q.RemoveStocks(ctx, sqlc.RemoveStocksParams{
+			Count: stock.Count,
+			Sku:   stock.Sku,
+		}); err != nil {
+			return fmt.Errorf("remove stocks: %w", err)
+		}
+	}
+
+	if err := q.DeleteReservedStockByUserID(ctx, pgtype.Int8{
+		Int64: int64(userID),
+		Valid: true,
+	}); err != nil {
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
+
+func (r *StocksRepository) ReserveCancel(
+	ctx context.Context,
+	userID models.UserID,
+	items []models.ItemOrderInfo,
+) error {
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+
+	if err := q.DeleteReservedStockByUserID(ctx, pgtype.Int8{
+		Int64: int64(userID),
+		Valid: true,
+	}); err != nil {
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
+
+func (r *StocksRepository) GetStocksBySKU(
+	ctx context.Context,
+	SKU models.SKU,
+) (count uint64, err error) {
+	tx, err := r.dbpool.Begin(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q := sqlc.New(r.dbpool)
+	q = q.WithTx(tx)
+
+	stocksCount, err := q.CountStocksBySKU(ctx, pgtype.Int8{
+		Int64: int64(SKU),
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	reservedStock, err := q.CountReservedStocksBySKU(ctx, pgtype.Int8{
+		Int64: int64(SKU),
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(stocksCount.Int64 - reservedStock), tx.Commit(ctx)
+}
