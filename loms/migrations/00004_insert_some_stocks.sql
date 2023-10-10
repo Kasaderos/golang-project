b37bf02890e2f5aa785130f02b52c103e963f802
Berdibek <kberda99@gmail.com>
2023-10-10 12:06:45 +0600

    [GOHW-3]: add goose tags

--- loms/migrations/00004_insert_some_stocks.sql
+++ loms/migrations/00004_insert_some_stocks.sql
@@ -1,3 +1,5 @@
+-- +goose Up
+-- +goose StatementBegin
 insert into stock(sku, available) 
 values (1076963, 1000),
        (1148162, 1000),
@@ -9,3 +11,9 @@ values (1076963, 1000),
        (3618852, 1000),
        (4288068, 1000),
        (4465995, 1000);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+truncate table stock;
+-- +goose StatementEnd
