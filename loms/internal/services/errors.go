6a9e125991fb81f300700ed2cc0dfa92c5e144df
Berdibek <kb.kaldarov@yandex.ru>
2023-10-21 23:55:25 +0600

    [GOHW-5]: remove dto, errors

--- loms/internal/services/errors.go
+++
@@ -1,9 +0,0 @@
-package usecase
-
-import "errors"
-
-var (
-	// todo add errors and wraps
-	ErrReserveStocks = errors.New("reserve stocks failed")
-	ErrCreateOrder   = errors.New("create order failed")
-)
