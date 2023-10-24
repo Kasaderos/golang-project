922ebc9c65358b59affc42b5b325136c8492546b
Berdibek <berdibek.kaldarov@mechta.kz>
2023-10-24 11:11:59 +0600

    [GOHW-5]: fix list item test
    - minimock isn't thread safe,
    do not run as t.Parallel(),
    last 2 tests read and writes cartItems.

--- cart/internal/services/cart/list_item_test.go
+++ cart/internal/services/cart/list_item_test.go
@@ -127,7 +127,6 @@ func TestCart_ListItem(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			s := NewListItemService(
 				tt.itemsProviderMock,
 				tt.productProviderMock,
