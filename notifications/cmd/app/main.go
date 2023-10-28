fd320d95250e8e07273540b237057fa2a1215666
Berdibek <kb.kaldarov@yandex.ru>
2023-10-28 22:25:25 +0600

    [GOHW-6]: add notification service

--- notifications/cmd/app/main.go
+++ notifications/cmd/app/main.go
@@ -1,5 +1,97 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"route256/loms/pkg/kafka"
+	"sync"
+	"syscall"
 
+	"github.com/Shopify/sarama"
+)
+
+const (
+	topicName       = "order-statuses-topic"
+	consumerGroupID = "consumer-group-id"
+)
+
+var brokers = []string{
+	"127.0.0.1:9091",
+	"127.0.0.1:9092",
+	"127.0.0.1:9093",
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Наш обработчик реализующий интерфейс sarama.ConsumerGroupHandler
+	consumerGroupHandler := NewConsumerGroupHandler()
+
+	// Создаем коньюмер группу
+	consumerGroup, err := kafka.NewConsumerGroup(
+		brokers,
+		consumerGroupID,
+		[]string{topicName},
+		consumerGroupHandler,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// запускаем вычитку сообщений
+		consumerGroup.Run(ctx)
+	}()
+
+	<-consumerGroupHandler.Ready() // Await till the consumer has been set up
+	log.Println("Sarama consumer up and running!...")
+
+	sigusr1 := make(chan os.Signal, 1)
+	signal.Notify(sigusr1, syscall.SIGUSR1)
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+
+	var (
+		consumptionIsPaused = false
+		keepRunning         = true
+	)
+	for keepRunning {
+		select {
+		case <-ctx.Done():
+			log.Println("terminating: context cancelled")
+			keepRunning = false
+		case <-sigterm:
+			log.Println("terminating: via signal")
+			keepRunning = false
+		case <-sigusr1:
+			toggleConsumptionFlow(consumerGroup, &consumptionIsPaused)
+		}
+	}
+
+	cancel()
+	wg.Wait()
+
+	if err = consumerGroup.Close(); err != nil {
+		log.Fatalf("Error closing consumer group: %v", err)
+	}
+}
+
+func toggleConsumptionFlow(cg sarama.ConsumerGroup, isPaused *bool) {
+	if *isPaused {
+		cg.ResumeAll()
+		log.Println("Resuming consumption")
+	} else {
+		cg.PauseAll()
+		log.Println("Pausing consumption")
+	}
+
+	*isPaused = !*isPaused
 }
